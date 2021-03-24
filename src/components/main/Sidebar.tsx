080d6af07578793950388a85ea7ec8b5074bb270
saubun <dameon0430@gmail.com>
2021-03-24 08:00:36 -0400

    lots of sidebar done

--- src/components/main/Sidebar.tsx
+++ src/components/main/Sidebar.tsx
@@ -2,8 +2,36 @@ import React from 'react';
 
 export default function Navbar() {
 	return (
-		<div>
-			<h1>test</h1>
+		<div className="sidebar">
+			<nav className="sidebar-nav">
+				<a className="nav-home" href="/">
+					<span>Home</span>
+				</a>
+				<a className="nav-explore" href="/">
+					<span>Explore</span>
+				</a>
+				<a className="nav-ubscriptions" href="/">
+					<span>Subscriptions</span>
+				</a>
+				<div className="fake-hr" />
+				<a className="nav-library" href="/">
+					<span>Library</span>
+				</a>
+				<a className="nav-history" href="/">
+					<span>History</span>
+				</a>
+				<a className="nav-your-videos" href="/">
+					<span>Your Videos</span>
+				</a>
+				<a className="nav-liked-videos" href="/">
+					<span>Liked Videos</span>
+				</a>
+				<a className="nav-show-more" href="/">
+					<span>Show More</span>
+				</a>
+				<div className="fake-hr" />
+				<h4 className="subscriptions-title">SUBSCRIPTIONS</h4>
+			</nav>
 		</div>
 	);
 }
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -4,12 +4,8 @@ import Sidebar from '../main/Sidebar';
 export default function App() {
 	return (
 		<>
-			<div id="nav-main-container">
-				<Navbar />
-			</div>
-			<div id="sidebar-main-container">
-				<Sidebar />
-			</div>
+			<Navbar />
+			<Sidebar />
 		</>
 	);
 }
